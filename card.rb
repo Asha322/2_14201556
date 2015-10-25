0dd55d6cb6537f848a3872fcdf26a1efaba4749c
Asha322 <ashfaris94@gmail.com>
2015-10-25 17:21:22 +0000

    initialize and initialize_random methods

--- card.rb
+++ card.rb
@@ -3,9 +3,13 @@ class Card
 
   attr_reader :rank
 
-  def initialize
+  def initialize_random
     init_array = %w{Two Three Four Five Six Seven Eight Nine Ten Jack Queen King Ace}
     @rank = init_array[rand(13)]
+  end
+
+  def initialize(s)
+    @rank = s
   end
 
   def to_s
