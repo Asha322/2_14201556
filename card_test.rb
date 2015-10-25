c63e40c8b8966677bca730485b00f3b20fc0d142
Asha322 <ashfaris94@gmail.com>
2015-10-25 14:51:15 +0000

    card_test basic func

--- card_test.rb
+++ card_test.rb
@@ -1,13 +1,12 @@
 require 'test/unit'
-require 'card.rb'
+require_relative 'card.rb'
 
 class Card_test < Test::Unit::TestCase
   def setup
     @c1 = Card.new
   end
-  def print
+  def test_initialize
     init_array = %w{Two Three Four Five Six Seven Eight Nine Ten Jack Queen King Ace}
-    #b = init_array.include?(@c1.to_s)
     assert_equal(true, init_array.include?(@c1.to_s), 'not in array')
   end
 end
