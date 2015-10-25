d9bce0e7e7122617c47073663b84115e6ab7e981
Asha322 <ashfaris94@gmail.com>
2015-10-25 14:50:40 +0000

    to_s

--- card.rb
+++ card.rb
@@ -1,11 +1,11 @@
 class Card
   @rank
 
-  attr_reader :@rank
+  attr_reader :rank
 
   def initialize
     init_array = %w{Two Three Four Five Six Seven Eight Nine Ten Jack Queen King Ace}
-    @rank = init_array[rand(13)]
+    @rank = init_array[rand(12)]
   end
 
   def to_s
