59b82c906e183d69924b09b3bfcf9e22b68671e6
Asha322 <ashfaris94@gmail.com>
2015-10-25 15:36:02 +0000

    card_test improved

--- card_test.rb
+++ card_test.rb
@@ -7,6 +7,16 @@ class Card_test < Test::Unit::TestCase
   end
   def test_initialize
     init_array = %w{Two Three Four Five Six Seven Eight Nine Ten Jack Queen King Ace}
-    assert_equal(true, init_array.include?(@c1.to_s), 'not in array')
+    for i in 0..10 do
+      @c1 = Card.new
+      assert_equal(true, init_array.include?(@c1.to_s), 'not in array')
+    end
+  end
+  def test_rand       #this test should sometimes fail and it does
+    for i in 0..5 do
+      @c3 = Card.new
+      @c4 = Card.new
+      assert_not_equal(true, @c3.to_s.eql?(@c4.to_s), 'equal')
+    end
   end
 end
