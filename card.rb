ca6fcc92e8d105225b2776ab6c73e0a88b5aaab0
Asha322 <ashfaris94@gmail.com>
2015-10-25 17:35:25 +0000

    initialize_random and tests for random cards removed

--- card.rb
+++ card.rb
@@ -3,11 +3,6 @@ class Card
 
   attr_reader :rank
 
-  def initialize
-    init_array = %w{Two Three Four Five Six Seven Eight Nine Ten Jack Queen King Ace}
-    @rank = init_array[rand(13)]
-  end
-
   def initialize(s)
     @rank = s
   end
