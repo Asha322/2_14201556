ff56ce96349bb53df274f0c68fa88a08ebd86cac
Asha322 <ashfaris94@gmail.com>
2015-10-26 13:50:28 +0000

    Spider class partially tested, hand updated

--- hand_test.rb
+++ hand_test.rb
@@ -18,20 +18,31 @@ class Hand_test < Test::Unit::TestCase
   end
 
   def test_add_card
-    @spider.add_card(Card.new("Nine"))
-    assert_equal(true, @spider.contains?("Nine"), 'value not added')
+    nine = Card.new("Nine")
+    two = Card.new("Two")
+    ace = Card.new("Ace")
+    king = Card.new("King")
+    ten = Card.new("Ten")
+
+    #test Spider class
+
+    @spider.add_card(nine)
+    assert_equal(true, @spider.contains?(nine), 'value not added')
     assert_not_equal(true, @spider.complete?, 'incorrect evaluation')
 
-    @spider.add_card(Card.new("Two"))
-    @spider.add_card(Card.new("Ace"))
-    @spider.add_card(Card.new("King"))
-    @spider.add_card(Card.new("Nine"))
+    @spider.add_card(two)
+    assert_equal(true, @spider.contains?(two), 'value not added')
+    assert_not_equal(true, @spider.complete?, 'incorrect evaluation')
+    assert_equal(true, @spider.contains?(nine), 'value removed')
+    assert_not_equal(true, @spider.complete?, 'incorrect evaluation')
+
+    @spider.add_card(ace)
+    @spider.add_card(king)
+    @spider.add_card(nine)
     assert_not_equal(true, @spider.complete?, 'incorrect evaluation')
     assert_not_equal(5, @spider.cards.size, 'extra value added')
 
-    @spider.add_card(Card.new("Queen"))
-    assert_equal(true, @spider.complete?, 'incorrect evaluation')
-
-
+    @spider.add_card(ten)
+    assert_equal(5, @spider.cards.size, 'extra value added')
   end
 end
